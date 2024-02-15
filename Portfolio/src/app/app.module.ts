ca3f2b19bcf913e210e8305ad952bdf0be5e214d
unknown <benomranesalim1@gmail.com>
2024-02-15 20:13:33 +0100

    first commit

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { ExperiencePageComponent } from './components/experience-page/experience-page.component';
 import { ExperienceBoxComponent } from './components/experience-page/experience-box/experience-box.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DescriptionPageComponent } from './components/description-page/description-page.component';
 
 @NgModule({
-  declarations: [AppComponent, ExperiencePageComponent, ExperienceBoxComponent],
+  declarations: [AppComponent, ExperiencePageComponent, ExperienceBoxComponent, DescriptionPageComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
