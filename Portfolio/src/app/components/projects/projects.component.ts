8a1d384178f0c9d50eb3f341e4cab386c6b75a47
ibar09 <iheb30abdelkader@gmail.com>
2024-02-18 15:28:12 +0100

    modal

--- Portfolio/src/app/components/projects/projects.component.ts
+++ Portfolio/src/app/components/projects/projects.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
+  display: string;
+  constructor(private modalService: NgbModal) {
+    this.display = 'none';
+  }
 
+  openModal() {
+    this.display = 'block';
+  }
+  onCloseHandled() {
+    this.display = 'none';
+  }
+  openVerticallyCentered(content: TemplateRef<any>) {
+    this.modalService.open(content, {
+      centered: true,
+      modalDialogClass: 'dark-modal',
+    });
+  }
 }
