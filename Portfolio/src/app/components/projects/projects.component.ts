02f8e96cc3a978eed7ca0cae6276ee16e832ae47
MedSalimBenOmrane <benomranesalim1@gmail.com>
2024-02-18 15:26:22 +0100

    projects

--- Portfolio/src/app/components/projects/projects.component.ts
+++ Portfolio/src/app/components/projects/projects.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import {  project } from 'src/app/model/project';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  projects: project[];
+  constructor() {
+    this.projects=[
+            new project("god of war 1","game dev","unity","2022","","description god of war ",[],[]),
+            new project("god of war 2","game dev","unity","2022","","description god of war ",[],[]),
+            new project("god of war 3","game dev","unity","2022","","description god of war ",[],[]),
+            new project("god of war 4","game dev","unity","2022","","description god of war ",[],[]),
+            new project("god of war 5","game dev","unity","2022","","description god of war ",[],[]),
+            new project("god of war 6","game dev","unity","2022","","description god of war ",[],[]),
+            new project("god of war 7","game dev","unity","2022","","description god of war ",[],[]),
+            new project("god of war 8","game dev","unity","2022","","description god of war ",[],[]),
+            new project("god of war 9","game dev","unity","2022","","description god of war ",[],[]),
+
+
+
+
+    ]
+  }
 
 }
