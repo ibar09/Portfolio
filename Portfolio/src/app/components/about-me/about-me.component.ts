41a9e2643fcad43221d4fb131df7ce554c64fd32
ibar09 <iheb30abdelkader@gmail.com>
2024-02-20 11:48:41 +0100

    cv button

--- Portfolio/src/app/components/about-me/about-me.component.ts
+++ Portfolio/src/app/components/about-me/about-me.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.css']
+  styleUrls: ['./about-me.component.css'],
 })
 export class AboutMeComponent {
-
+  downloadPdf() {
+    const pdfUrl = 'assets/CV.pdf'; // Update with your local PDF file path
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'resume.pdf';
+    link.click();
+  }
 }
